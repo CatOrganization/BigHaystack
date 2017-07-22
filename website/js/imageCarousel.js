21ec4d311f909fc87d69baaa4b99ef496e6a56e0
Ricky Barillas <ricky.barillas@gmail.com>
2017-07-22 17:59:27 -0400

    need to rebase

---
+++ website/js/imageCarousel.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	var carouselContainer = $('#divImageCarousel');
+	// The wrapper is contained within carouselContainer
+	var carouselWrapper = $('#divCarouselWrapper');
+	var indicatorList = $('#olIndicators');
+	var carouselSlides = $('#divCarouselSlides')
+
+	var palceholderDir = "../images/carousel_images/";
+	var images = ["placeholder1", "placeholder2", "placeholder3"];
+
+	for (i in images) {
+		
+
+	}
+
+
+});
+
+function createImageSlide() {
+	var image = palceholderDir + images[i];
+	var slide = document.createElement("DIV");
+	var imageDiv = document.createElement("DIV");
+	var 
+	slide.setAttribute("class", "item active");
+}
