87753a2bc223aa2a77f5b3e6877cb459e6a4965f
Ricky Barillas <ricky.barillas@gmail.com>
2017-07-22 19:11:33 -0400

    Got carousel to work

--- website/js/imageCarousel.js
+++ website/js/imageCarousel.js
@@ -3,23 +3,38 @@ $(document).ready(function() {
 	// The wrapper is contained within carouselContainer
 	var carouselWrapper = $('#divCarouselWrapper');
 	var indicatorList = $('#olIndicators');
-	var carouselSlides = $('#divCarouselSlides')
+	var carouselSlides = $('#divCarouselSlides');
 
-	var palceholderDir = "../images/carousel_images/";
-	var images = ["placeholder1", "placeholder2", "placeholder3"];
+	var placeholderDir = 'img/carousel_images/';
+	var images = ['placeholder1.jpg', 'placeholder2.jpg', 'placeholder3.jpg'];
 
-	for (i in images) {
-		
-
+	for (var i = 0; i < images.length; i++) {
+		carouselSlides.append(createImageSlide(placeholderDir + images[i], i + 1));
+		indicatorList.append(createIndicator(i + 1))
 	}
+
+	$('#divSlide1').toggleClass('active');
+	$('#liIndicator1').toggleClass('active');
 
 
 });
 
-function createImageSlide() {
-	var image = palceholderDir + images[i];
-	var slide = document.createElement("DIV");
-	var imageDiv = document.createElement("DIV");
-	var 
-	slide.setAttribute("class", "item active");
+function createImageSlide(imageSrc, imageNum) {
+	var slide = document.createElement('DIV');
+	var image = document.createElement('IMG');
+	slide.setAttribute('class', 'carousel-item');
+	image.setAttribute('src', imageSrc);
+	slide.setAttribute('id', 'divSlide' + imageNum);
+
+	slide.appendChild(image);
+	return slide;
+}
+
+function createIndicator(indicatorNum) {
+	var indicator = document.createElement('LI');
+	indicator.setAttribute('data-target', '#divCarouselWrapper');
+	// offset this back to zero indexed
+	indicator.setAttribute('data-slide-to', indicatorNum - 1);
+	indicator.setAttribute('id', 'liIndicator' + indicatorNum)
+	return indicator;
 }
